a6f4a662974108412c7a3cc18c50ff1fdb7e7b8c
okorkov <alex.okorkov@gmail.com>
2021-05-12 14:48:18 -0700

    galleries added and filled with pictures

--- src/components/ProjectsGallery.jsx
+++ src/components/ProjectsGallery.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
 
-const ProjectsGallery = () => {
-  return (
-    <div id="projects">
-      Projects Gal
-    </div>
-  );
+import React, { Component } from "react";
+import DztImageGalleryComponent from "reactjs-image-gallery";
+
+class ProjectsGallery extends Component {
+  render() {
+    let data = [
+      {
+        url:
+          "https://live.staticflickr.com/65535/51176096225_d716485ec6_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51176096225_4837ff4194_n.jpg"
+      },
+      {
+        url:
+          "https://live.staticflickr.com/65535/51176096120_1e537282ac_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51176096120_c74289056a_n.jpg"
+      }
+    ];
+
+    return (
+      <div id="projects">
+        <p className="category">Projects</p>
+        <DztImageGalleryComponent images={data} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>
+      </div>
+    );
+  }
 }
 
 export default ProjectsGallery;
--- src/components/WoodGallery.jsx
+++ src/components/WoodGallery.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
 
-export default function WoodGallery() {
-  return (
-    <div id="wood">
-      Wood Gal
-    </div>
-  )
+import React, { Component } from "react";
+import DztImageGalleryComponent from "reactjs-image-gallery";
+
+class WoodGallery extends Component {
+  render() {
+    let data = [
+      {
+        url:
+          "https://live.staticflickr.com/65535/51175791824_b657477398_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51175791824_7b332d4141_n.jpg"
+      },
+      {
+        url:
+          "https://live.staticflickr.com/65535/51175227383_c95e478f97_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51175227383_9a34940ffa_n.jpg"
+      },
+      {
+        url:
+          "https://live.staticflickr.com/65535/51175004506_e19e74ac4e_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51175004506_5cae18b8d8_n.jpg"
+      },
+      {
+        url:
+          "https://live.staticflickr.com/65535/51175227428_bb9c008d08_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51175227428_bb050976a8_n.jpg"
+      },
+
+      {
+        url:
+          "https://live.staticflickr.com/65535/51175227438_34d651dc71_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51175227438_5d27097ccc_n.jpg"
+      },
+      {
+        url:
+          "https://live.staticflickr.com/65535/51174328417_3e098cfb06_o.jpg",
+        
+        thumbUrl:
+          "https://live.staticflickr.com/65535/51174328417_812e0fe2d5_n.jpg"
+      }
+    ];
+
+    return (
+      <div id="wood">
+        <p className="category">Our Wood</p>
+        <DztImageGalleryComponent images={data} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>
+      </div>
+    );
+  }
 }
+
+export default WoodGallery;
