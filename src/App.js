50e725bcfed50410362fb90bc9c77551da44f1fe
okorkov <alex.okorkov@gmail.com>
2021-05-11 20:08:39 -0700

    libraries update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Header from './components/Header';
-import Pages from './components/Pages';
+
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Pages />
+      
       <Footer />
     </div>
   );
