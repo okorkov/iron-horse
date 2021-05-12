8227821bd76d9ed1c4951287a1abc6e8b60a0434
okorkov <alex.okorkov@gmail.com>
2021-05-12 13:49:21 -0700

    added about me content

--- src/index.js
+++ src/index.js
@@ -26,6 +26,11 @@ document.addEventListener("DOMContentLoaded", function(){
   });
 }); 
 
+
+
+
+
+
 document.getElementsByClassName('_R2FsI')[0].addEventListener('click', (e) => {
   window.location.href = "#";
 })
