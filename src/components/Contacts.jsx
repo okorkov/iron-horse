33e6801602306464b363a98ad2b938c1be3994fa
okorkov <alex.okorkov@gmail.com>
2021-05-13 13:45:50 -0700

    contact form added

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
 
 
-const Contacts = () => {
 
+
+  const Contacts = () => {
+  
   const email = 'address@gmail.com';
+  
+  const [subject, setSubject] = React.useState({subject: ''});
+  const [body, setBody] = React.useState({body: ''});
+  let link = `mailto:${email}?subject=${subject.subject}&body=${body.body}`;
 
-  return (
-    <div id="contacts">
-      <input type="email" maxlength="50" placeholder="Title..." value="" />
-      <textarea rows="8" maxlength="500" placeholder="Contents..." />
-      <a href="mailto:address@gmail.com?subject=Hello there&amp;body=I wanted to ask" target="_blank" rel="noopener noreferrer">Send E-mail</a>
-    </div>
-  );
-}
+  const handleSubjectInput = (e) => {
+    setSubject({
+      [e.target.name]: e.target.value
+    })
+    link = `mailto:${email}?subject=${subject.subject}&body=${body.body}`;
+  }
 
-export default Contacts;
+  const handleBodyInput = (e) => {
+    setBody({
+      [e.target.name]: e.target.value
+    })
+    link = `mailto:${email}?subject=${subject.subject}&body=${body.body}`;
+  }
+
+    return (
+      <div id="contacts">
+          <label className="about-text-hello" >Send us an email!</label>
+          <input className="input" type="email" maxlength="50" placeholder="Title..." name="subject"  value={subject.subject} onChange={(e) => handleSubjectInput(e)}/>
+          <textarea className="input" rows="8" maxlength="500" placeholder="Contents..." name="body"  value={body.body} onChange={(e) => handleBodyInput(e)}/>
+          <a href={link} target="_blank" rel="noopener noreferrer"><button className="button">Send E-mail</button></a>
+
+        </div>
+      );
+  }
+
+  export default Contacts;
+
+
