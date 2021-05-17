d4a91be9e167252fb0d6e97951de9aa5826afdd7
okorkov <alex.okorkov@gmail.com>
2021-05-17 09:52:13 -0700

    fixed buttom styling on sending an email

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -30,7 +30,7 @@ import React from 'react';
           <label className="about-text-hello" >Send us an email!</label>
           <input className="input" type="email" maxlength="50" placeholder="Title..." name="subject"  value={subject.subject} onChange={(e) => handleSubjectInput(e)}/>
           <textarea className="input" rows="8" maxlength="500" placeholder="Contents..." name="body"  value={body.body} onChange={(e) => handleBodyInput(e)}/>
-          <a href={link} target="_blank" rel="noopener noreferrer"><button className="button">Send</button></a>
+          <a href={link} target="_blank" rel="noopener noreferrer"><button className="button btn">Send</button></a>
 
         </div>
       );
