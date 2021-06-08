74a21b896e8148409995d1c7046bb807e074cdd9
okorkov <alex.okorkov@gmail.com>
2021-06-08 10:43:25 -0700

    added version to the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Package from '../../package.json'
+
 
 const Footer = () => {
   return (
     <footer className="text-lg-start ">
     <div className="text-center p-3" >
       <p>© {new Date().getFullYear()} Iron Horse Studio</p>
+      <p>Version {Package.version}</p>
     </div>
   </footer>
   )
