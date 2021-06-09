c65298214a83b081583f8177e54e6ae5758e4a67
okorkov <alex.okorkov@gmail.com>
2021-06-08 20:12:17 -0700

    yelp api test

--- src/App.js
+++ src/App.js
@@ -16,8 +16,21 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://ironhorsestudio.herokuapp.com/api')
+    axios.get('https://cors-anywhere.herokuapp.com/https://ironhorsestudio.herokuapp.com/api')
     .then(data => this.setState({ isLoaded: true, data: data.data}))
+    var myHeaders = new Headers();
+    myHeaders.append("Authorization", "Bearer qeFH5UPL9KU6OyCNvfeb6lkRxBrHvDY-HQ0uGH9nqH7PCTOO7IE9Vk-K4PYAGyNqq3IChAcFu35PbKhw4D9003qmSQhicplRzn_YKFzJH7hCi7xsOl9KhJzJLQvAYHYx");
+    
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+    };
+    
+    fetch("https://api.yelp.com/v3/businesses/iron-horse-studio-fairfax/reviews", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
   }
 
   render(){
