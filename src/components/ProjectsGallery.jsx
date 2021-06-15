dfec31bd193e41c10311e21f7bc58eb06a6869bb
okorkov <alex.okorkov@gmail.com>
2021-06-15 10:15:14 -0700

    changed galleries from accordeon to state rendering

--- src/components/ProjectsGallery.jsx
+++ src/components/ProjectsGallery.jsx
@@ -1,15 +1,12 @@
 
 import React, { Component } from "react";
 import DztImageGalleryComponent from "reactjs-image-gallery";
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-
 class ProjectsGallery extends Component {
 
   state = {
-    images: []
+    images: [],
+    loadMore: false
   }
 
   componentDidMount() {
@@ -18,77 +15,35 @@ class ProjectsGallery extends Component {
     }
   }
 
+  loadMoreImages() {
+    document.getElementById('projects-gal').remove()
+    this.setState({
+      ...this.state,
+      loadMore: true
+    })
+    window.scrollBy(0, 1); // scrolls page by 1 pixel to get css update to the rest of the images
+  }
+
+  renderImages() {
+    return(
+      <>
+      <p className="category">Projects</p>
+      {(this.state.loadMore)?
+      <DztImageGalleryComponent images={this.state.images.map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>:
+      <DztImageGalleryComponent images={this.state.images.slice(0,4).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/> }
+      <div id="projects-gal" style={{textAlign: 'center', fontSize: '18px', color: '#858484', textDecoration: 'underline'}}
+        onClick={() => this.loadMoreImages()}>
+        <ExpandMoreIcon />
+      </div>
+      </>
+    )
+  }
+
   render() {
-    let backupData = [
-      {
-        url:
-          "https://live.staticflickr.com/65535/51201125973_52a75abe1e_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51201125973_c79569ea0d_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51201700654_5d2bcfa4ca_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51201700654_2e911e8cff_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51201985930_6abecffe59_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51201985930_3a1f476566_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51200923141_8ddd319221_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51200923141_8474032c67_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51200922946_616f3ca665_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51200922946_2bb92e5def_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51201985795_7134134c65_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51201985795_52fab5e744_m.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51200922681_33e92b2410_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51200922681_0b9c662fb5_m.jpg"
-      }
-    ];
 
     return (
       <div id="projects">
-        <p className="category">Projects</p>
-        {(this.props.data)?
-        <Accordion>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <DztImageGalleryComponent images={this.state.images.slice(0,4).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/> 
-          </AccordionSummary>
-          <AccordionDetails>
-          <DztImageGalleryComponent images={this.state.images.slice(4, this.state.images.length).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/> 
-          </AccordionDetails>
-          </Accordion> 
-        : 
-        <DztImageGalleryComponent images={backupData} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/> }
+        {this.renderImages()}
       </div>
     );
   }
--- src/components/WoodGallery.jsx
+++ src/components/WoodGallery.jsx
@@ -1,16 +1,14 @@
 
 import React, { Component } from "react";
 import DztImageGalleryComponent from "reactjs-image-gallery";
-import Accordion from '@material-ui/core/Accordion';
-import AccordionSummary from '@material-ui/core/AccordionSummary';
-import AccordionDetails from '@material-ui/core/AccordionDetails';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 
 class WoodGallery extends Component {
 
   state = {
-    images: []
+    images: [],
+    loadMore: false
   }
 
   componentDidMount() {
@@ -19,65 +17,35 @@ class WoodGallery extends Component {
     }
   }
 
-  render() {
-    let backupData = [
-      {
-        url:
-          "https://live.staticflickr.com/65535/51175791824_b657477398_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51175791824_7b332d4141_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51175227428_bb9c008d08_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51175227428_bb050976a8_n.jpg",
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51174328417_3e098cfb06_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51174328417_812e0fe2d5_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51175227438_34d651dc71_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51175227438_5d27097ccc_n.jpg"
-      },
-      {
-        url:
-          "https://live.staticflickr.com/65535/51175227383_c95e478f97_o.jpg",
-        
-        thumbUrl:
-          "https://live.staticflickr.com/65535/51175227383_9a34940ffa_n.jpg"
-      },
-    ];
+  loadMoreImages() {
+    document.getElementById('wood-gal').remove()
+    this.setState({
+      ...this.state,
+      loadMore: true
+    })
+    window.scrollBy(0, 1); // scrolls page by 1 pixel to get css update to the rest of the images
+  }
 
+  renderImages() {
+    return(
+      <>
+        <p className="category">Our Wood</p>
+       {(this.state.loadMore)?
+        <DztImageGalleryComponent images={this.state.images.map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>:
+        <DztImageGalleryComponent images={this.state.images.slice(0,4).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>}
+          <div id="wood-gal" style={{textAlign: 'center', fontSize: '18px', color: '#858484', textDecoration: 'underline'}}
+          onClick={() => this.loadMoreImages()}>
+          <ExpandMoreIcon />
+        </div>
+      </>
+    )
+  }
+
+  render() {
+   
     return (
       <div id="wood">
-        <p className="category">Our Wood</p>
-        {(this.props.data)?
-        <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <DztImageGalleryComponent images={this.state.images.slice(0,4).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>
-        </AccordionSummary>
-        <AccordionDetails>
-          <DztImageGalleryComponent images={this.state.images.slice(4, this.state.images.length).map(e => ({ url: e, thumbUrl: e }))} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>
-        </AccordionDetails>
-        </Accordion> 
-        :
-        <DztImageGalleryComponent images={backupData} hideRotate hideDownload hideZoom imageBackgroundColor='#e4e4e8'/>
-        }
-        
+        {this.renderImages()}
       </div>
     );
   }
