aa6bcdbebb57443d8967746aefd5f20074666bda
juan_carlos_meza <juancmezat@gmail.com>
2021-06-16 02:52:28 -0700

    switched pictures

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,12 +43,12 @@ class About extends Component {
     owned wood business based in Fairfax, California. We specialize in
     custom, reclaimed wood projects from sustainable, local sources. It is a
     blessing to live in Northern California, with such a wide variety of species.`},
-    {image: this.props.project_pic, text:`We also specialize in esoteric species from around the world. All of our
+    {image: this.props.wood_pic, text:`We also specialize in esoteric species from around the world. All of our
     wood is one hundred percent reclaimed, and handpicked by our
     specialists; only the top twenty percent making the cut. Most of our
     projects are custom; created to the client's specification, and completely
     tailored to your liking!`},
-    {image: this.props.wood_pic, text:`We always have a nice, rotating inventory as well if
+    {image: this.props.project_pic, text:`We always have a nice, rotating inventory as well if
     you would like to do your own project. We are a full-service wood mill,
     and offer planing, sanding, and milling services; collaborating with a local
     Master Metalsmith if needed. Feel free to reach out, and let's talk about
