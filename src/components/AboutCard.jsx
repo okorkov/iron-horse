07ec441fc3747edb5b9fa409194c883c4cde7e8e
Juan Carlos Meza <juancmezat@gmail.com>
2021-06-15 19:56:31 -0700

    model card finished

--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -4,8 +4,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import TextInfoContent from '@mui-treasury/components/content/textInfo';
 import { useBlogTextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/blog';
 import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
 
@@ -16,24 +14,25 @@ const useStyles = makeStyles(({ breakpoints, spacing }) => ({
     transition: '0.3s',
     boxShadow: '0px 14px 30px rgba(34, 35, 58, 0.2)',
     position: 'relative',
-    maxWidth: 750,
+    maxWidth: 780,
     marginLeft: 'auto',
     overflow: 'initial',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     paddingBottom: spacing(2),
+    paddingTop: spacing(2),
     [breakpoints.up('md')]: {
       flexDirection: 'row',
       paddingTop: spacing(2),
     },
   },
   media: {
-    minWidth: '58%',
+    minWidth: '50%',
     marginLeft: 'auto',
     marginRight: 'auto',
     marginTop: spacing(-1),
-    paddingBottom: '48%',
+    paddingBottom: '52%',
     borderRadius: spacing(2),
     backgroundColor: '#fff',
     position: 'relative',
@@ -76,10 +75,6 @@ const useStyles = makeStyles(({ breakpoints, spacing }) => ({
 
 export const BlogCardDemo = React.memo(function BlogCard({data}) {
   const styles = useStyles();
-  const {
-    button: buttonStyles,
-    ...contentStyles
-  } = useBlogTextInfoContentStyles();
   const shadowStyles = useOverShadowStyles();
   return (
     <Card className={cx(styles.root, shadowStyles.root)}>
