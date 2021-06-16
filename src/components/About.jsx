86d0f61049792b13b2d09dd0aa79a75bc31504c9
Juan Carlos Meza <juancmezat@gmail.com>
2021-06-16 00:34:36 -0700

    carousel rotates automatically

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,6 +9,27 @@ class About extends Component {
     slideNumber: 0,
   }
 
+  componentDidMount() {
+    this.kickOffCarousel()
+  }
+
+  moveToNextSlide = () => {
+    this.state.slideNumber === 2 ?
+    this.resetCarousel()
+    :
+    this.setState(prevstate => ({ slideNumber: prevstate.slideNumber + 1}))
+    }
+
+  kickOffCarousel = () => {
+      setInterval(this.moveToNextSlide, 4500)
+  }
+
+  resetCarousel = () => {
+    clearInterval(this.kickOffCarousel)
+    this.setState({slideNumber: 0})
+  }
+
+
   slides = [
     {image: this.props.data.about_image, text:`Thanks for stopping by! We are a small, full service, family-
     owned wood business based in Fairfax, California. We specialize in
@@ -17,44 +38,25 @@ class About extends Component {
     {image: this.props.project_pic, text:`We also specialize in esoteric species from around the world. All of our
     wood is one hundred percent reclaimed, and handpicked by our
     specialists; only the top twenty percent making the cut. Most of our
-    projects are custom; created to the client&#39;s specification, and completely
+    projects are custom; created to the client's specification, and completely
     tailored to your liking!`},
     {image: this.props.wood_pic, text:`We always have a nice, rotating inventory as well if
     you would like to do your own project. We are a full-service wood mill,
     and offer planing, sanding, and milling services; collaborating with a local
-    Master Metalsmith if needed. Feel free to reach out, and let&#39;s talk about
+    Master Metalsmith if needed. Feel free to reach out, and let's talk about
     your next project!`},
+    {image: this.props.data.about_image, text:`Thanks for stopping by! We are a small, full service, family-
+    owned wood business based in Fairfax, California. We specialize in
+    custom, reclaimed wood projects from sustainable, local sources. It is a
+    blessing to live in Northern California, with such a wide variety of species.`}
   ]
 
-render(){
-  return (
-    <div id="about" >
-      {/* <img src={(props.data)? props.data.about_image: "https://live.staticflickr.com/65535/51175695010_8173e223cb_c.jpg"}
-        className="avatar" alt="Charles Gehrig"
-      />
-      <p className='about-text-hello'>Hello!</p>
-      <p className="about-text">
-          { (props.data)?
-            props.data.about_text:
-            `Thanks for stopping by! We are a small, full service, family-
-            owned wood business based in Fairfax, California. We specialize in
-            custom, reclaimed wood projects from sustainable, local sources. It is a
-            blessing to live in Northern California, with such a wide variety of species.
-            We also specialize in esoteric species from around the world. All of our
-            wood is one hundred percent reclaimed, and handpicked by our
-            specialists; only the top twenty percent making the cut. Most of our
-            projects are custom; created to the client&#39;s specification, and completely
-            tailored to your liking! We always have a nice, rotating inventory as well if
-            you would like to do your own project. We are a full-service wood mill,
-            and offer planing, sanding, and milling services; collaborating with a local
-            Master Metalsmith if needed. Feel free to reach out, and let&#39;s talk about
-            your next project!`
-          }
-        </p> */}
-        <AboutCard data={this.slides[this.state.slideNumber]}></AboutCard>
-        {console.log(this.props)}
-    </div>
-  )
+  render(){
+    return (
+      <div id="about" >
+          <AboutCard data={this.slides[this.state.slideNumber]}></AboutCard>
+      </div>
+    )
   };
 }
 
