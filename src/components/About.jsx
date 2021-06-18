fa3bb56f80b73acbb09c89cfdca25b18fa70abb3
juan_carlos_meza <juancmezat@gmail.com>
2021-06-17 19:02:34 -0700

    accepted incoming changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -44,26 +44,9 @@ class About extends Component {
 
 
   slides = [
-<<<<<<< HEAD
-    {image: this.state.pics[0], text:`Thanks for stopping by! We are a small, full service, family-
-    owned wood business based in Fairfax, California. We specialize in
-    custom, reclaimed wood projects from sustainable, local sources. It is a
-    blessing to live in Northern California, with such a wide variety of species.`},
-    {image: this.state.pics[1], text:`We also specialize in esoteric species from around the world. All of our
-    wood is one hundred percent reclaimed, and handpicked by our
-    specialists; only the top twenty percent making the cut. Most of our
-    projects are custom; created to the client's specification, and completely
-    tailored to your liking!`},
-    {image: this.state.pics[2], text:`We always have a nice, rotating inventory as well if
-    you would like to do your own project. We are a full-service wood mill,
-    and offer planing, sanding, and milling services; collaborating with a local
-    Master Metalsmith if needed. Feel free to reach out, and let's talk about
-    your next project!`},
-=======
     {image: this.props.data.about_image, text: this.props.data.about_text.slice(0,278)},
     {image: this.props.wood_pic, text: this.props.data.about_text.slice(278,538)},
     {image: this.props.project_pic, text: this.props.data.about_text.slice(538,this.props.data.about_text.length) },
->>>>>>> 10478658e362d0865fda923d6fe7b0dae35fecd6
   ]
 
   render(){
